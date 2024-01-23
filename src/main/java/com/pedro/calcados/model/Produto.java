0fb6cb7581de96134eed07b31781f18ef8f9398d
Santiago <victor.lacsilva0102@gmail.com>
2024-01-23 20:00:40 -0300

    feat: added @Column annotation to normalize attribute name to db standards

--- src/main/java/com/pedro/calcados/model/Produto.java
+++ src/main/java/com/pedro/calcados/model/Produto.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +23,8 @@ public class Produto {
   private Long id;
 
   private Double preco;
+
+  @Column(name = "quantidade_estoque")
   private Long quantidadeEstoque;
   private List<Integer> tamanhos;
 
