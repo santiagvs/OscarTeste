4bf8e89f8d518c5ffd2e4ba40ea277f9f955a291
Santiago <victor.lacsilva0102@gmail.com>
2024-01-25 19:37:48 -0300

    feat: added new method for ProdutoRespository for testing purposes

--- src/main/java/com/pedro/calcados/repository/ProdutoRepository.java
+++ src/main/java/com/pedro/calcados/repository/ProdutoRepository.java
@@ -1,11 +1,16 @@
 package com.pedro.calcados.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import com.pedro.calcados.model.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>, JpaSpecificationExecutor<Produto> {
-
+  @Query("SELECT p FROM Produto p WHERE p.modelo.id = ?1")
+  List<Produto> findByModeloId(Long id);
 }
