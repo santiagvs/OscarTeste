9d82c1495e0d38d70d291c93969b7eb31846f772
Santiago <victor.lacsilva0102@gmail.com>
2024-01-25 16:44:42 -0300

    feat: added new test to FindAll marcas

--- src/test/java/com/pedro/calcados/repository/MarcaRepositoryTests.java
+++ src/test/java/com/pedro/calcados/repository/MarcaRepositoryTests.java
@@ -18,8 +18,7 @@ public class MarcaRepositoryTests {
   @Test
   public void MarcaRepository_Save_ReturnsSavedMarca() {
     // Arrange
-    Marca marca = new Marca();
-    marca.setNome("Nike");
+    Marca marca = Marca.builder().nome("Nike").build();
 
     // Act
     Marca savedMarca = marcaRepository.save(marca);
@@ -27,5 +26,22 @@ public class MarcaRepositoryTests {
     // Assert
     Assertions.assertThat(savedMarca).isNotNull();
     Assertions.assertThat(savedMarca.getId()).isGreaterThan(0);
+  }
+
+  @Test
+  public void MarcaRepository_FindAll_ReturnsListOfMarcas() {
+    // Arrange
+    Marca marca1 = Marca.builder().nome("Nike").build();
+    Marca marca2 = Marca.builder().nome("Adidas").build();
+    marcaRepository.save(marca1);
+    marcaRepository.save(marca2);
+
+    // Act
+    Iterable<Marca> marcas = marcaRepository.findAll();
+
+    // Assert
+    Assertions.assertThat(marcas).isNotEmpty();
+    Assertions.assertThat(marcas).hasSize(2);
+    Assertions.assertThat(marcas).element(0).hasFieldOrPropertyWithValue("nome", "Nike");
   }
 }
